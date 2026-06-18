b4e5205409c6145d1c072e68445eb45894d97e75
Matias Amaya <65874039+serugeneris@users.noreply.github.com>
2026-06-18 14:56:54 -0300

    feat: add vite config to allow connecting via local clints (eg. test phone)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173, // You can specify a port or leave it default
+  },
 })
